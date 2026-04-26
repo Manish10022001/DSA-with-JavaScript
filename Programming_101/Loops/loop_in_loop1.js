9a7cdf0da90c3e1f747a13c8cf995d9635b5cd48
Manish10022001 <manishshirsat2850@gmail.com>
2026-04-26 23:59:47 +0530

    Loop in Loop

--- Programming_101/Loops/loop_in_loop1.js
+++ Programming_101/Loops/loop_in_loop1.js
@@ -1,5 +1,13 @@
-for (let i = 0; i < 5; i++) {
-  for (let j = 0; j < 5; j++) {
-    console.log(i, j);
-  }
+//program 1:
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j < 5; j++) {
+//     console.log(i, j);
+//   }
+// }
+
+//porgram 2:
+for(let i=0; i<5; i++){
+    for(let j = 0; j<i; j++){
+        console.log(i,j);
+    }
 }
