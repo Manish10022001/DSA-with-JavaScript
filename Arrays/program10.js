9ec86c00f6708a10f607d97f8aaef6bd92be05b8
Manish10022001 <manishshirsat2850@gmail.com>
2026-05-08 00:09:12 +0530

    Arrays: Reverse String - array of characters

--- Arrays/program10.js
+++ Arrays/program10.js
@@ -4,12 +4,18 @@ Write a function that reverses a string. The input string is given as an array o
 */
 
 let reverseString = function (s) {
-  for (let i = 0; i < n / 2; i++) {
-    let temp = s[i];
+
+  let n = s.length; //to get length of array
+  let half = Math.floor(n / 2);
+
+  //   for (let i = 0; i < n / 2; i++) {
+  for (let i = 0; i < half; i++) {
+    //swap [i, n-1-i]
+    let temp = s[i]; //use variable to store the value of a to later use to replace
     s[i] = s[n - 1 - i];
     s[n - 1 - i] = temp;
   }
 };
-let num= ['a','d','d','i','p'];
-let result = reverseString(num);
-console.log(result);
+let num = ["a", "d", "d", "i", "p"];
+reverseString(num);
+console.log(num);
