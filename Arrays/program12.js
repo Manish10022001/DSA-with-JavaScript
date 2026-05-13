143e4c7ca3eb6251ecf688ada5bc018297286df3
Manish10022001 <manishshirsat2850@gmail.com>
2026-05-13 20:06:55 +0530

    Arrays: Merge Sorted Arrays: Optimal approach without extra space

--- Arrays/program12.js
+++ Arrays/program12.js
@@ -6,23 +6,43 @@ Mergenums1 and nums2 into a single array sorted in non-decreasing order.
 
 The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 */
+//Approach 3: Optimal Approach: without extra space
+let merge = function(num1, m, num2, n){
+  let p1 = m-1;
+  let p2 = n-1;
+  
+  for(let i=m+n-1; i>=0; i--){
+    if(p2<0) break;
+    if(num1[p1] > num2[p2] &&  p1>=0){
+      num1[i] = num1[p1];
+      p1--;
+    } else{
+      num1[i] = num2[p2];
+      p2--;
+    }
+  }
+}
+let num1 = [1,2,3];
+let num2 = [2,4,8];
+
+merge(num1,3,num2,3)
+console.log(num1);
 
 //Approach 2: Using two pointers
-let merge = function (num1, m, num2, n) {
+/*let merge = function (num1, m, num2, n) {
   let nCopy = num1.slice(0, m);
   let p1 = 0;
   let p2 = 0;
 
   for (let i = 0; i < m + n; i++) {
-    /*
-     Take element from nCopy if:
-      1. nCopy still has remaining elements
-      AND
-      2. Either:
-         - current nCopy element is smaller
-         OR
-         - all num2 elements are already used
-    */
+    // 
+    //  Take element from nCopy if:
+    //   1. nCopy still has remaining elements
+    //   AND
+    //   2. Either:
+    //      - current nCopy element is smaller
+    //      OR
+    //      - all num2 elements are already used
     if ((p1 < m && nCopy[p1] <= num2[p2]) || p2 >= n) {
       num1[i] = nCopy[p1];
       p1++;
@@ -36,6 +56,8 @@ let num1 = [1, 2, 3, 0, 0, 0];
 let num2 = [2, 4, 5];
 merge(num1, 3, num2, 3);
 console.log(num1);
+*/
+
 
 //Approach 1: Brute Force
 /*
