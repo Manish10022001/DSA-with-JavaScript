ed8feb8d3006cb71a20c97ac6c244101581dc14c
Manish10022001 <manishshirsat2850@gmail.com>
2026-05-13 19:43:22 +0530

    Arrays: Merge Sorted Arrays: Two Pointer Method

--- Arrays/program12.js
+++ Arrays/program12.js
@@ -7,7 +7,38 @@ Mergenums1 and nums2 into a single array sorted in non-decreasing order.
 The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 */
 
+//Approach 2: Using two pointers
+let merge = function (num1, m, num2, n) {
+  let nCopy = num1.slice(0, m);
+  let p1 = 0;
+  let p2 = 0;
+
+  for (let i = 0; i < m + n; i++) {
+    /*
+     Take element from nCopy if:
+      1. nCopy still has remaining elements
+      AND
+      2. Either:
+         - current nCopy element is smaller
+         OR
+         - all num2 elements are already used
+    */
+    if ((p1 < m && nCopy[p1] <= num2[p2]) || p2 >= n) {
+      num1[i] = nCopy[p1];
+      p1++;
+    } else {
+      num1[i] = num2[p2];
+      p2++;
+    }
+  }
+};
+let num1 = [1, 2, 3, 0, 0, 0];
+let num2 = [2, 4, 5];
+merge(num1, 3, num2, 3);
+console.log(num1);
+
 //Approach 1: Brute Force
+/*
 let merge = function(num1, m, num2, n){
     //merge m2 into m1, so start from index m, num1.lenght = 6, so we need till index 5,
     for(let i = m ; i<num1.length; i++){
@@ -20,3 +51,4 @@ let num1 = [1,2,3,0,0,0];
 let num2 = [2,4,5];
 merge(num1,3, num2,3);
 console.log(num1);
+*/
